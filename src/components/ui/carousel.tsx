3f1e3a99cdc20f1a6a48eed419d6ce5f53881429
Ian Duke <induke@dons.usfca.edu>
2025-11-09 15:55:17 -0800

    Change carousel max width from lg to md

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -95,7 +95,7 @@ export const Carousel = React.forwardRef<
       <div
         ref={ref}
         {...props}
-        className={cn("max-w-lg", className)}
+        className={cn("max-w-md", className)}
         role="region"
         aria-roledescription="carousel"
       >
